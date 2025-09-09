309df33b946afc453b46640b896d58fd728d2c5f
NChekFR <n.chekhovskoy@mail.utoronto.ca>
2025-09-09 12:13:21 -0400

    readme, fixxbuzz and main updates

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -6,36 +6,39 @@ package lab1;
 class FizzBuzz {
 
     public static void main(String[] args) {
-        int i = 100;
-        fizzbuzzwhile(i);
-    }
-
-    public static void fizzbuzzwhile(int i) {
-        while(i < 100) {
-
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+        int i = 1;
+        while (i < 100) {
+            doFizzBuzz(i);
             i++;
         }
+
+    }
+
+    public static void doFizzBuzz(int i) {
+
+
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+        i++;
     }
 }
